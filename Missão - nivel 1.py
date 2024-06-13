56db6908cc914012a982e868ed9a8e4e0a2b5d00
Rafaldm <rafaitu2009@hotmail.com>
2024-06-13 00:53:29 -0300

    first commit

---
+++ Missão - nivel 1.py
@@ -0,0 +1,18 @@
+# Cadastro de dados #
+saudação = ("Olá")
+print("Olá,")
+inicio = input("Digite: ok e tecle enter para continuar: ")
+print("Perfeito, agora vamos coletar alguns dados!")
+nome = input("Digite seu nome: ")
+print("seja bem vindo! " + nome)
+idade = input("digite sua idade: ")
+altura = input("digite sua altura: ")
+peso = input("digite seu peso: ")
+input("confirme seus dados abaixo, digite ok e pressione enter para continuar: ")
+print(nome)
+print(idade)
+print(altura+"cm")
+print(peso+"KG")
+input("para confirmar digite ok: ")
+print("Seus dados foram registrados com sucesso, obrigado!")
+
