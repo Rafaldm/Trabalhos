be112f6f4eeef7db5920f2048a7c28894567fcdd
Rafaldm <rafaitu2009@hotmail.com>
2024-06-15 01:57:30 -0300

    microatividade_nivel_2

---
+++ micro_atividade2.py
@@ -0,0 +1,6 @@
+#Micro atividade nivel 2#
+print ("Olá eu estou conhecendo a linguagem python")
+print ("Meu nome é Rafael e tenho", 15 + 14, "anos")
+print (1 + 1)
+print (7 + 15 - 25 / 8)
+print ("Fim de código")
