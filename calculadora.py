58c092da3cde98b3021cee02504378102aafb9b4
Rafaldm <rafaitu2009@hotmail.com>
2024-06-15 04:45:07 -0300

    missão_pratica

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+a1 = int(input('digite o valor 1: '))
+a2 = int(input('digite o valor 2: '))
+
+print ("""
+    Resultado soma:{} 
+    Resultado subtração:{} 
+    Resultado divisão:{} 
+    Resultado multiplicação:{} """
+    .format(a1+a2, a1-a2, a1*a2, a1//a2))
